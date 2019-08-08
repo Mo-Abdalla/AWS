a8fd4a0728a98764ee0e236e81de2eef5d95b66c
Mo-Abdalla <mohamed.abdalla3141@gmail.com>
2019-08-08 09:36:21 +0000

    updated title

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_D4GYMOoEj', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1j10d0qem4lus1cnhkdiph5n4u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1b' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
